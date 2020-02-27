7c308f73db78e47c042efcb64ee2729b2a490957
moeKiwiSAMA <me@kiwi.cat>
2020-02-27 14:18:45 +0800

    Commit Bot At Thu Feb 27 14:18:45 2020

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@ import os
 import time
 
 
-while True:
-    with open("commits/logs", "a") as logs:
+if __name__ == "__main__":
+    while True:
+        with open("commits/logs", "a") as logs:
             logs.writelines(time.asctime() + "\n")
     
-    os.system("git add .")
-    os.system("git commit -m \"Commit Bot At " + time.asctime() + "\"")
-    os.system("git push")
+        os.system("git add .")
+        os.system("git commit -m \"Commit Bot At " + time.asctime() + "\"")
+        os.system("git push")
 
-    time.sleep(600)
+        time.sleep(600)
